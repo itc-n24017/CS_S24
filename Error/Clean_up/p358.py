14d94462b4af925b8e0917973a413375a4b83f5b
itc-n24017 <n24017@std.it-college.ac.jp>
2024-08-13 05:31:47 +0000

    ８月１３日

---
+++ Error/Clean_up/p358.py
@@ -0,0 +1,15 @@
+def test_function():
+    try:
+        print('try')
+        return
+    except:
+        print('except')
+    else:
+        print('else')
+    finally:
+        print('finally')
+
+test_function()
+
+
+#答えは１番
