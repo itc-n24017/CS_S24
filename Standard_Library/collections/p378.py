b9e3c1e7f4a33d6083b32badc5f7e3e30af567c5
itc-n24017 <n24017@std.it-college.ac.jp>
2024-08-14 05:09:23 +0000

    ８月１４日

---
+++ Standard_Library/collections/p378.py
@@ -0,0 +1,8 @@
+import collections
+data = 'すもももももももものうち'
+count = collections.Counter(data)
+res_dict = collections.defaultdict(list)
+for ch, cnt in count. items():
+    res_dict[cnt].append(ch)
+res_dict[1]
+
