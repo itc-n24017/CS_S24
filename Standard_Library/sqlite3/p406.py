bb97d3d37f7cc36eee43d970aff4e7ef3840100f
itc-n24017 <n24017@std.it-college.ac.jp>
2024-08-15 05:29:02 +0000

    ８月１５日

--- Standard_Library/sqlite3/p406.py
+++ Standard_Library/sqlite3/p406.py
@@ -1,23 +1,3 @@
-import sqlite3
-
-db = 'test.db'
-con = sqlite3.connect(db)
-csr = con.cursor()
-
-csr.execute('create table human(name, nickname);')
-
-csr.execute("insert into human(name) values('Ichiro');")
-csr.execute("insert into human(name) values('Jiro');")
-csr.execute("insert into human(name) values('Saburo');")
-
-csr.execute("update human set nickname = 'Lazer' where name = 'Ichiro';")
-
-csr.execute("select * from human;")
-print(csr.fetchall())
-
-con.commit()
-con.close()
-
 #p406の回答
 [('Ichiro', 'Lazer'), ('Jiro', None), ('Saburo', None)]
 
